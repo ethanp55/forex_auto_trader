97c996e70b1b345198d5baaf5d6e1961426663bd
Ethan Pedersen <ethanp55@gmail.com>
2020-12-04 21:49:58 -0800

    Update beep boop model

--- Model/beep_boop.py
+++ Model/beep_boop.py
@@ -7,6 +7,8 @@ class BeepBoop(object):
     def predict(self, currency_pair, current_data, n_open_trades):
         beep_boop1 = current_data.loc[current_data.index[-1], 'beep_boop']
         beep_boop2 = current_data.loc[current_data.index[-2], 'beep_boop']
+        beep_boop3 = current_data.loc[current_data.index[-3], 'beep_boop']
+        beep_boop4 = current_data.loc[current_data.index[-4], 'beep_boop']
         ema200 = current_data.loc[current_data.index[-1], 'ema200']
         bid_low1 = current_data.loc[current_data.index[-1], 'Bid_Low']
         bid_high1 = current_data.loc[current_data.index[-1], 'Bid_High']
@@ -16,6 +18,8 @@ class BeepBoop(object):
         print('New data for: ' + str(currency_pair))
         print('beep_boop1: ' + str(beep_boop1))
         print('beep_boop2: ' + str(beep_boop2))
+        print('beep_boop3: ' + str(beep_boop3))
+        print('beep_boop4: ' + str(beep_boop4))
         print('ema200: ' + str(ema200))
         print('bid_low1: ' + str(bid_low1))
         print('bid_high1: ' + str(bid_high1))
@@ -23,10 +27,10 @@ class BeepBoop(object):
         print('bid_close1: ' + str(bid_close1))
         print()
 
-        if float(bid_low1) > float(ema200) and beep_boop2 == 0 and beep_boop1 == 1 and n_open_trades < self.max_open_trades and bid_open1 < bid_close1:
+        if float(bid_low1) > float(ema200) and beep_boop4 == 1 and beep_boop3 == 1 and beep_boop2 == 1 and beep_boop1 == 1 and n_open_trades < self.max_open_trades and bid_open1 < bid_close1:
             trade = 'buy'
 
-        elif float(bid_high1) < float(ema200) and beep_boop2 == 0 and beep_boop1 == 2 and n_open_trades < self.max_open_trades and bid_open1 > bid_close1:
+        elif float(bid_high1) < float(ema200) and beep_boop4 == 2 and beep_boop3 == 2 and beep_boop2 == 2 and beep_boop1 == 2 and n_open_trades < self.max_open_trades and bid_open1 > bid_close1:
             trade = 'sell'
 
         else:
