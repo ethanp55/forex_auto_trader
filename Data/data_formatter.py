047254fbb4c5100a858510fff80aac3932d3badd
Ethan Pedersen <ethanp55@gmail.com>
2021-02-02 16:31:24 -0800

    Add temporary print message to double check data types

--- Data/data_formatter.py
+++ Data/data_formatter.py
@@ -153,4 +153,6 @@ class DataFormatter(object):
         df = df.append(last_three_rows, ignore_index=True)
         df.reset_index(drop=True, inplace=True)
 
+        print(df.dtypes)
+
         return df
